d881fa1deb306c05eca1312c83e1d69af2d32fb2
elektroNIK <bogdan.kalinin@gmail.com>
2018-02-20 16:07:14 +0200

    Delete track button.

--- P365/urls.py
+++ P365/urls.py
@@ -22,7 +22,7 @@ from profile import urls as profile_urls
 from calendar_year import urls as calendar_urls
 from maps import urls as map_urls
 
-from maps.views import TracksView, TrackChangeStatusView, GetTracksTableView, TrackEditView
+from maps.views import TracksView, TrackChangeStatusView, GetTracksTableView, TrackEditView, TrackDeleteView
 from profile.views import LoginView, ProfileRedirect, SignUpView, IndexView
 from story.views import StoriesView
 
@@ -44,8 +44,10 @@ urlpatterns = [
 
     url(r'^track/(\d+)/change_status/$',
         login_required(TrackChangeStatusView.as_view()),            name='change_track_status'),
-    url(r'^get_tracks_table/$',
-        login_required(GetTracksTableView.as_view()),               name='get_tracks_table'),
+    url(r'^track/(\d+)/delete/$',
+        login_required(TrackDeleteView.as_view()),                  name='delete_track'),
     url(r'^track/(\d+)/edit/$',
         login_required(TrackEditView.as_view()),                    name='edit_track'),
+    url(r'^get_tracks_table/$',
+        login_required(GetTracksTableView.as_view()),               name='get_tracks_table'),
 ]
