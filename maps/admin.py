cf21d5d8349c086e1740c0a35ac992865940e4d8
elektroNIK <bogdan.kalinin@gmail.com>
2018-02-20 16:12:34 +0200

    Admin fix for new models.

--- maps/admin.py
+++ maps/admin.py
@@ -5,15 +5,15 @@ from . import models
 
 
 class POIAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'created', 'user',)
-    list_filter = ('user',)
+    list_display = ('name', 'created', 'user', 'is_active',)
+    list_filter = ('user', 'is_active',)
     ordering = ('-created',)
     search_fields = ('name', 'description',)
 
 
 class TrackAdmin(LeafletGeoAdmin):
-    list_display = ('name', 'created', 'start_date', 'finish_date', 'public', 'length', 'user',)
-    list_filter = ('user', 'public',)
+    list_display = ('name', 'created', 'start_date', 'finish_date', 'activity', 'public', 'is_active', 'length', 'user',)
+    list_filter = ('user', 'activity', 'public', 'is_active',)
     ordering = ('-created',)
     search_fields = ('name', 'description',)
 
