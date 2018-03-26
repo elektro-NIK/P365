037d6cc3951480935c7eacd8c06644dbd64ed166
elektroNIK <bogdan.kalinin@gmail.com>
2018-03-26 18:01:43 +0300

    Refactoring + security

--- profile/views.py
+++ profile/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import View
 
@@ -19,36 +20,28 @@ class LoginView(View):
     @staticmethod
     def get(request):
         if request.user.is_authenticated:
-            return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user.username}))
+            return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user}))
         else:
-            from django.contrib.auth.forms import AuthenticationForm
             form = AuthenticationForm()
             nexturl = request.GET.get('next') or ''
             return render(request, 'login.html', {'title': 'Login', 'form': form, 'next': nexturl})
 
     @staticmethod
     def post(request):
-        from django.contrib.auth.forms import AuthenticationForm
         form = AuthenticationForm(data=request.POST)
         nexturl = request.GET.get('next')
         if form.is_valid():
-            user = User.objects.get(username=request.POST['username'])
-            login(request, user)
-            return HttpResponseRedirect(nexturl or reverse('profile', kwargs={'username': user.username}))
+            login(request, request.user)
+            return HttpResponseRedirect(nexturl or reverse('profile', kwargs={'username': request.user}))
         else:
-            return render(request, 'login.html', {
-                'title': 'Login',
-                'form': form,
-                'next': nexturl,
-                'active': 'login',
-            })
+            return render(request, 'login.html', {'title': 'Login', 'form': form, 'next': nexturl})
 
 
 class SignUpView(View):
     @staticmethod
     def get(request):
         if request.user.is_authenticated:
-            return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user.username}))
+            return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user}))
         else:
             form = SignUpForm()
             nexturl = request.GET.get('next') or ''
@@ -61,26 +54,21 @@ class SignUpView(View):
         if form.is_valid():
             form.save()
             user = authenticate(request, username=request.POST['username'], password=request.POST['password1'])
-            if user is not None:
+            if user:
                 login(request, user)
             return HttpResponseRedirect(nexturl or reverse('profile', kwargs={'username': user.username}))
         else:
-            return render(request, 'register.html', {
-                'title': 'Sign Up',
-                'form': form,
-                'next': nexturl,
-                'active': 'signup',
-            })
+            return render(request, 'register.html', {'title': 'Sign Up', 'form': form, 'next': nexturl})
 
 
 class ProfileRedirect(LoginRequiredMixin, View):
     @staticmethod
     def get(request):
-        return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user.username}))
+        return HttpResponseRedirect(reverse('profile', kwargs={'username': request.user}))
 
 
 class ProfileView(LoginRequiredMixin, View):
     @staticmethod
     def get(request, username):
-        profile = User.objects.get(username=username)
+        profile = get_object_or_404(User, username=username)
         return render(request, 'profile.html', {'title': 'Profile', 'profile': profile})
