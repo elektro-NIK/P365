463821dced016bb4f454896afda27fef3ee5feee
elektroNIK <bogdan.kalinin@gmail.com>
2018-09-03 16:30:41 +0300

    Added the function of removing features on the map.

--- static/js/map.js
+++ static/js/map.js
@@ -1,18 +1,40 @@
+function delete_feature (map, feature, url) {
+    var token = $('[name=csrfmiddlewaretoken]').val()
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+            csrfmiddlewaretoken: token,
+        },
+        success: function() {
+            map.eachLayer(function (layer) {
+                if (layer.feature == feature)
+                    map.removeLayer(layer);
+            });
+        },
+        error: function() {
+            setErrorMsg();
+        }
+    });
+}
+
+function addPopup (map, feature, layer, url, url_edit, url_delete) {
+    var popup = $('<p>').append($('<a>').attr('href', url.replace('0', feature.properties.pk)).html($('<b>').html(feature.properties.name)), ' ');
+    if (url_edit)
+        popup.append($('<a>').attr('href', url_edit.replace('0', feature.properties.pk)).addClass('glyphicon glyphicon-pencil'), ' ');
+    if (!feature.properties.public)
+        popup.append($('<a>').attr('href', '#').addClass('glyphicon glyphicon-trash').on('click', function() {
+            delete_feature(map, feature, url_delete.replace('0', feature.properties.pk));
+        }));
+    popup.append($('<br>'), (feature.properties.description || ''));
+    layer.bindPopup(popup[0]);
+}
+
 function parseData (map, data, url, url_edit, url_delete) {
     var json = JSON.parse(data);
     b = L.geoJson(json, {
-        onEachFeature: function (feature, layer) {
-            var popup =
-            '<p>' +
-                '<a href="' + url.replace('0', feature.properties.pk) + '">' +
-                    '<b>' + feature.properties.name + '</b>' +
-                '</a>';
-            if (url_edit)
-                popup += ' <a href="' + url_edit.replace('0', feature.properties.pk) + '" class="glyphicon glyphicon-pencil"></a>'
-            if (url_delete)
-                popup += ' <a href="#" class="glyphicon glyphicon-trash"></a>'
-            popup += '<br>' + (feature.properties.description || '') + '</p>';
-            layer.bindPopup(popup);
+        onEachFeature: function(feature, layer) {
+            addPopup(map, feature, layer, url, url_edit, url_delete);
             layer.on('click', function() {
                 this.feature.geometry.type != 'Point' ? map.fitBounds(this.getBounds()) : {};
             });
@@ -33,8 +55,7 @@ function get_all (map, url_ids, url_get, url_show, url_edit, url_delete) {
             }
             $('.progress').show();
             for (i = 0; i < data.length; i++) {
-                var id = data[i];
-                var url = url_get.replace('0', id);
+                var url = url_get.replace('0', data[i]);
                 var step = 1./3/data.length;
                 $.getJSON(url)
                 .done(function(data) {
@@ -46,12 +67,11 @@ function get_all (map, url_ids, url_get, url_show, url_edit, url_delete) {
                     }
                     parseData(map, data, url_show, url_edit, url_delete);
                 })
-                .fail(function()     {setErrorMsg()});
+                .fail(function() {setErrorMsg()});
             }
         }
-        else {
-            progress += 1./3
-        }
+        else
+            progress += 1./3;
     })
     .fail(function() {setErrorMsg()});
 }
