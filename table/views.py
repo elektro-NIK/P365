13e6b4c933cbd5457f38e08b566c95526b32104f
elektroNIK <bogdan.kalinin@gmail.com>
2018-09-28 17:10:09 +0300

    Fixed tracks table after change status

--- table/views.py
+++ table/views.py
@@ -78,25 +78,10 @@ class TableView(View):
         return HttpResponseRedirect(reverse('table:view'))
 
 
-class UpdateTablesView(View):
-    @staticmethod
-    def get(request):
-        pois = POIModel.objects.filter(user=request.user, is_active=True).order_by('-created')
-        routes = RouteModel.objects.filter(user=request.user, is_active=True).order_by('-created')
-        tracks = TrackModel.objects.filter(user=request.user, is_active=True).order_by('-start_date')
-        return render(request, '_tables.html', {
-            'title': 'Table',
-            'active': 'table',
-            'pois': pois,
-            'routes': routes,
-            'tracks': tracks,
-        })
-
-
 @method_decorator(login_required, name='dispatch')
 class UpdateTableView(View):
     @staticmethod
     def get(request, model, template, temp_var_name):
-        order = '-start_date' if isinstance(model, TrackModel) else '-created'              # Tracks order by start date
+        order = '-start_date' if model == TrackModel else '-created'              # Tracks order by start date
         objs = model.objects.filter(user=request.user, is_active=True).order_by(order)
         return render(request, template, {temp_var_name: objs})
