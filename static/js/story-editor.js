5818a7183dca2487001edc9438537feaf2f56c36
elektroNIK <bogdan.kalinin@gmail.com>
2018-11-26 09:18:23 +0200

    Refactoring story-editor and story-viewer html and js

--- static/js/story-editor.js
+++ static/js/story-editor.js
@@ -23,6 +23,43 @@ function map_init_basic (map, options) {
     });
 }
 
+$("select[name='track']").on('change', function() {
+    var map = mapsPlaceholder.pop();
+    map.eachLayer(function (layer) {
+        if (!layer._url)
+            map.removeLayer(layer);
+    });
+    if (this.value) {
+        url = url_track_json.replace("0", this.value)
+        $.getJSON(url)
+        .done(function (data) {
+            var track = L.geoJson(JSON.parse(data), {
+                onEachFeature: function(feature, layer) {
+                    layer.bindPopup(feature.properties.description || '');
+                }
+            }).addTo(map);
+            bounds = track.getBounds();
+            map.fitBounds(bounds);
+        })
+        .fail(function() {setErrorMsg()});
+    }
+    mapsPlaceholder.push(map);
+});
+
+$("select[name='event']").on('change', function() {
+    $("#event-dates").html("");
+    if (this.value) {
+        url = url_calendar_dates.replace("0", this.value)
+        $.ajax({
+            url: url,
+            success: function (data) {
+                $("#event-dates").html(data);
+            },
+            error: function() {setErrorMsg()}
+        });
+    }
+});
+
 $(window).on('load', function() {
     $("select[name='track']").change();
     $("select[name='event']").change();
