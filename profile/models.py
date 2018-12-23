347f96dc223143089d6b48c64e46c0e138cdebc0
elektroNIK <bogdan.kalinin@gmail.com>
2018-12-23 20:35:30 +0200

    - changed location for avatars - display avatar in header - changed avatar DB model

--- profile/models.py
+++ profile/models.py
@@ -4,11 +4,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+def avatar_user_path(instance, filename):
+    return f'avatars/{instance.user.username}/{filename}'
+
+
 class ProfileModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     info = models.TextField(max_length=100, blank=True)
     location = models.CharField(max_length=30, blank=True)
-    pic = models.ImageField(upload_to='media/avatars/', default='media/avatars/default/default.png')
+    pic = models.ImageField(upload_to=avatar_user_path, default='media/avatars/default/default.png')
     fb_url = models.CharField(max_length=100, blank=True)
     tw_url = models.CharField(max_length=100, blank=True)
     ig_url = models.CharField(max_length=100, blank=True)
