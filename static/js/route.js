b2cfcc1020eeb2b919ddbcc83ab2e94bce2b3ee8
elektroNIK <bogdan.kalinin@gmail.com>
2019-03-15 10:49:04 +0200

    Migration to Bootstrap4: - route viewer

--- static/js/route.js
+++ static/js/route.js
@@ -7,19 +7,6 @@ function map_init_basic (map, options) {
     if (index)
         $("input[name='leaflet-base-layers']")[index].click();
 
-    var info_height = $('#info').height();
-    var page_height = $(document).height();
-    var window_height = $(window).height();
-    var header_height = $('#header_container').height() + parseInt($('#header_container').css('margin-bottom').replace('px', ''));
-    var footer_height = $('footer').height();
-    if (info_height + header_height >= window_height)
-        var height = window_height - header_height;
-    else if (info_height + header_height + footer_height <= window_height)
-        var height = page_height - header_height - footer_height - parseInt($('#header_container').css('margin-bottom').replace('px', '')) - 2;
-    else
-        var height = info_height;
-    $('div.leaflet-container').height(height);
-    map.invalidate;
     $.getJSON(url_route)
     .done(function (data) {
         var elevation = L.control.elevation({
