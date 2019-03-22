d460ed9ac7d66cfc69ba1a13c2a7027a8fe30051
elektroNIK <bogdan.kalinin@gmail.com>
2019-03-22 14:13:16 +0200

    Migration to Bootstrap4: - tag templates

--- tag/views.py
+++ tag/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
+from taggit.models import Tag
 
 from map.models import POIModel, RouteModel, TrackModel
 from story.models import StoryModel
@@ -17,31 +18,27 @@ class TagCloudView(View):
 class TagView(View):
     @staticmethod
     def get(request, tagslug):
+        tag = Tag.objects.get(slug=tagslug)
         stories = StoryModel.objects.filter(
-            Q(tags__slug=tagslug), Q(is_active=True)
+            Q(tags=tag), Q(is_active=True)
         ).order_by('-created')
         if request.user.is_anonymous:
-            pois = POIModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(public=True)
-            ).order_by('-created')
-            routes = RouteModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(public=True)
-            ).order_by('-created')
-            tracks = TrackModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(public=True)
-            ).order_by('-start_date')
+            pois = POIModel.objects.filter(tags=tag, is_active=True, public=True).order_by('-created')
+            routes = RouteModel.objects.filter(tags=tag, is_active=True, public=True).order_by('-created')
+            tracks = TrackModel.objects.filter(tags=tag, is_active=True, public=True).order_by('-start_date')
         else:
             pois = POIModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(user=request.user) | Q(public=True)
+                Q(tags=tag), Q(is_active=True), Q(user=request.user) | Q(public=True)
             ).order_by('-created')
             routes = RouteModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(user=request.user) | Q(public=True)
+                Q(tags=tag), Q(is_active=True), Q(user=request.user) | Q(public=True)
             ).order_by('-created')
             tracks = TrackModel.objects.filter(
-                Q(tags__slug=tagslug), Q(is_active=True), Q(user=request.user) | Q(public=True)
+                Q(tags=tag), Q(is_active=True), Q(user=request.user) | Q(public=True)
             ).order_by('-start_date')
         return render(request, 'tag.html', {
             'title': '#' + tagslug,
+            'tag': tag,
             'pois': pois,
             'routes': routes,
             'tracks': tracks,
